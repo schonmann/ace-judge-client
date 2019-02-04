afe2ba7880c86e02b48a44e7da5050a438b8b0ef
Antonio C. Schönmann Alves <antonio_schonmann@hotmail.com>
2019-02-04 16:52:47 -0200

    Layout improvements.

--- client/src/app/modules/problem-list/components/problem-list/problem-list.component.ts
+++ client/src/app/modules/problem-list/components/problem-list/problem-list.component.ts
@@ -17,6 +17,9 @@ export class ProblemListComponent implements OnInit {
   }, {
     label: "Sucesso %",
     field: "success",
+  }, {
+    label: "Nível",
+    field: "level",
   }];
 
   constructor() { }
@@ -31,34 +34,42 @@ export class ProblemListComponent implements OnInit {
           id: 1,
           name: "Be like Bumble",
           success: 14.75,
+          level: "Expert",
         }, {
           id: 2,
           name: "Sherlock and The Cost",
           success: 24.75,
+          level: "Hard",
         }, {
           id: 3,
           name: "Mathworks ",
           success: 33.75,
+          level: "Medium",
         }, {
           id: 4,
           name: "Cash-in that carry!",
           success: 43.75,
+          level: "Medium",
         }, {
           id: 5,
           name: "Crop-circles and tryangles",
           success: 92.12,
+          level: "Easy",
         }, {
           id: 6,
           name: "High and Risen",
           success: 22.53,
+          level: "Advanced",
         }, {
           id: 7,
           name: "Can you even type?",
           success: 1.33,
+          level: "Expert",
         }, {
           id: 8,
           name: "ICNS Assignment",
           success: 46.2,
+          level: "Medium",
         }].slice(pageIndex*pageSize, pageIndex*pageSize + pageSize),
         total: 8,
       });
