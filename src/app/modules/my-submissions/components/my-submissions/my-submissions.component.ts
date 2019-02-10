dbdc8e83f1de1cfa761f18cc3d02e6e2b5469a18
schonmann <antonio_schonmann@hotmail.com>
2019-02-10 21:21:55 -0200

    Fix fields.

--- src/app/modules/my-submissions/components/my-submissions/my-submissions.component.ts
+++ src/app/modules/my-submissions/components/my-submissions/my-submissions.component.ts
@@ -18,8 +18,11 @@ export class MySubmissionsComponent implements OnInit {
     label: "Resultado",
     field: "result",
   }, {
-    label: "Ver Mais",
-    field: "more",
+    label: "Competição",
+    field: "contest",
+  }, {
+    label: "Categoria",
+    field: "category",
   }];
 
   constructor() { }
@@ -36,84 +39,112 @@ export class MySubmissionsComponent implements OnInit {
           success: 14.75,
           level: "Expert",
           result: "Complexidade Incorreta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 2,
           name: "Sherlock and The Cost",
           success: 24.75,
           level: "Hard",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 3,
           name: "Mathworks ",
           success: 33.75,
           level: "Medium",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 4,
           name: "Cash-in that carry!",
           success: 43.75,
           level: "Medium",
           result: "Complexidade Incorreta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 5,
           name: "Crop-circles and tryangles",
           success: 92.12,
           level: "Easy",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 6,
           name: "High and Risen",
           success: 22.53,
           level: "Advanced",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 7,
           name: "Can you even type?",
           success: 1.33,
           level: "Expert",
           result: "Complexidade Incorreta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 8,
           name: "ICNS Assignment",
           success: 46.2,
           level: "Medium",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 9,
           name: "High and Risen",
           success: 22.53,
           level: "Advanced",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 10,
           name: "Can you even type?",
           success: 1.33,
           level: "Expert",
           result: "Complexidade Incorreta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 11,
           name: "ICNS Assignment",
           success: 46.2,
           level: "Medium",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 12,
           name: "High and Risen",
           success: 22.53,
           level: "Advanced",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 13,
           name: "Can you even type?",
           success: 1.33,
           level: "Expert",
           result: "Complexidade Incorreta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }, {
           id: 14,
           name: "ICNS Assignment",
           success: 46.2,
           level: "Medium",
           result: "Complexidade Correta",
+          contest: " - ",
+          category: "Programação Dinâmica"
         }].slice(pageIndex * pageSize, pageIndex * pageSize + pageSize),
         total: 14,
       });
