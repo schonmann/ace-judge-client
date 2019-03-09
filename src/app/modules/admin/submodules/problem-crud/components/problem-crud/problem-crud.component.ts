f2cb1691963ad9bba961328cda8f42a81d4d7795
schonmann <antonio_schonmann@hotmail.com>
2019-03-09 20:12:55 -0300

    Crud de problemas.

--- src/app/modules/admin/submodules/problem-crud/components/problem-crud/problem-crud.component.ts
+++ src/app/modules/admin/submodules/problem-crud/components/problem-crud/problem-crud.component.ts
@@ -1,26 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ProblemCategoryEnum } from 'src/app/shared/enum/ProblemCategoryEnum';
+import { ProblemDifficultyEnum } from 'src/app/shared/enum/ProblemDifficultyEnum';
+import { ProblemVisibilityEnum } from 'src/app/shared/enum/ProblemVisibilityEnum';
+import { ProblemCategoryHelper } from 'src/app/shared/helper/ProblemCategoryHelper';
+import { ProblemVisibilityHelper } from 'src/app/shared/helper/ProblemVisibilityHelper';
 import { Changeable } from 'src/app/shared/interface/changeable';
+import { ProblemDifficultyHelper } from 'src/app/shared/helper/ProblemDifficultyHelper';
 
 @Component({
   selector: 'app-problem-crud',
   templateUrl: './problem-crud.component.html',
   styleUrls: ['./problem-crud.component.scss']
 })
-export class ProblemCrudComponent implements OnInit, Changeable {
+export class ProblemCrudComponent implements Changeable {
 
-  private loading : boolean = false;
-  
+  private loading: boolean = false;
+  private categories: Array<any>;
+  private visibilities: Array<any>;
+  private difficulties: Array<any>;
+
 
   public Editor = ClassicEditor;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.categories = Object.keys(ProblemCategoryEnum).map(key => {
+      return {
+        name: ProblemCategoryHelper.getStatusNameByEnumValue(key),
+        value: key,
+      }
+    })
+    this.visibilities = Object.keys(ProblemVisibilityEnum).map(key => {
+      return {
+        name: ProblemVisibilityHelper.getStatusNameByEnumValue(key),
+        value: key,
+      }
+    })
+    this.difficulties = Object.keys(ProblemDifficultyEnum).map(key => {
+      return {
+        name: ProblemDifficultyHelper.getStatusNameByEnumValue(key),
+        value: key,
+      }
+    })
   }
 
   saveProblem(f: NgForm) {
+    console.log(f.form);
     if (f.invalid) {
       return;
     }
