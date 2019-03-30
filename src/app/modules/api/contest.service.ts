7aa207486cbc75195e4e8d0a866dbdf1151daff4
schonmann <antonio_schonmann@hotmail.com>
2019-03-30 13:43:14 -0300

    Pipe from array.

--- src/app/modules/api/contest.service.ts
+++ src/app/modules/api/contest.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, OperatorFunction } from 'rxjs';
 import { map, every } from 'rxjs/operators'
+import { pipeFromArray } from 'rxjs/internal/util/pipe';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,9 @@ export class ContestService {
 
   constructor(private http: HttpClient) { }
 
-  private contestTransform(): Array<OperatorFunction<any,any>> {
+  private contestTransform(): Array<OperatorFunction<any, any>> {
     return [
-      map((x : any) => {
+      map((x: any) => {
         x.startDate = new Date(x.startDate)
         x.endDate = new Date(x.endDate)
         return x
@@ -22,35 +23,38 @@ export class ContestService {
     ]
   }
 
-  getById(id: number) : Observable<Object> {
-    return this.http.get(`${this.baseUrl}/getById`, {
-      params: {
-        'id': id.toString()
-      }
-    }).pipe(...this.contestTransform());
+  getById(id: number): Observable<Object> {
+    return pipeFromArray([...this.contestTransform()])(
+      this.http.get(`${this.baseUrl}/getById`, {
+        params: {
+          'id': id.toString()
+        }
+      }))
   }
 
-  getByIdIfAuthorized(id: number) : Observable<Object> {
-    return this.http.get(`${this.baseUrl}/getByIdIfAuthorized`, {
-      params: {
-        'id': id.toString()
-      }
-    }).pipe(...this.contestTransform());
+  getByIdIfAuthorized(id: number): Observable<Object> {
+    return pipeFromArray([...this.contestTransform()])(
+      this.http.get(`${this.baseUrl}/getByIdIfAuthorized`, {
+        params: {
+          'id': id.toString()
+        }
+      }))
   }
 
   findByFilter(page: number, size: number): Promise<any> {
-    return this.http.get(`${this.baseUrl}/query`, {
-      params: {
-        'page': page.toString(),
-        'size': size.toString(),
-      }
-    }).pipe(...this.contestTransform()).toPromise()
+    return pipeFromArray([...this.contestTransform()])(
+      this.http.get(`${this.baseUrl}/query`, {
+        params: {
+          'page': page.toString(),
+          'size': size.toString(),
+        }
+      })).toPromise()
   }
 
   joinContest(contestId: number, password: string): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/join`, { 
+    return this.http.post(`${this.baseUrl}/join`, {
       contestId,
-      password 
+      password
     })
   }
 
