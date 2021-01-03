74f00d4b6deb493fa41942ea971119839b0805aa
schonmann <antonio_schonmann@hotmail.com>
2021-01-03 00:17:36 -0300

    Commiting

--- src/app/modules/my-submissions/components/submission-view/submission-view.component.ts
+++ src/app/modules/my-submissions/components/submission-view/submission-view.component.ts
@@ -22,6 +22,15 @@ export class SubmissionViewComponent implements OnInit {
 
   constructor(private submissionService : ProblemSubmissionService, private problemSubmissionService : ProblemSubmissionService, private router : Router, private route : ActivatedRoute, private toastrService : ToastrService) { }
 
+  private mapAnalysisFunctionToViewObject(fn) : any {
+    return {
+      expression: fn.expression,
+      parameters: fn.parameters,
+      error: fn.error.toFixed(3),
+      values: fn.values.map(x => x.toFixed(8)).join(', '),
+    };
+  }
+
   ngOnInit() {
     this.route.params.subscribe(async params => {
       const id = params['submissionId']
@@ -32,12 +41,12 @@ export class SubmissionViewComponent implements OnInit {
       this.categoryName = ProblemCategoryHelper.getStatusNameByEnumValue(this.submission.category);
       this.languageName = LanguageHelper.getStatusNameByEnumValue(this.submission.language);
       this.correctnessRuntime = `${this.submission.runtime.toFixed(3)}ms`;
-      const submitDate = new Date(this.submission.submitDate);
-      this.submitDate = submitDate.toLocaleTimeString();
-      this.analysisOutput = this.analysisOutput.reduce((prev, curr) => {
-        
-        return curr;
-      }, {});
+      this.submitDate = new Date(this.submission.submitDate).toLocaleString();
+      this.analysisOutput = this.submission.analysisOutput ? {
+        bestGuessFunction: this.mapAnalysisFunctionToViewObject(this.submission.analysisOutput.best_guess_function),
+        minimumErrorFunction: this.mapAnalysisFunctionToViewObject(this.submission.analysisOutput.minimum_error_function),
+        equivalentFunctions: this.submission.analysisOutput.equivalent_functions.map(this.mapAnalysisFunctionToViewObject),
+      } : null;
     })
   }
 }
